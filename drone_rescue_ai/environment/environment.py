be26fd177fcfec63af14bd825503cb839e950473
Evgenii Iurin <iurin.evgenii@gmail.com>
2024-11-05 23:24:09 +0100

    Align scripts

--- drone_rescue_ai/environment/environment.py
+++ drone_rescue_ai/environment/environment.py
@@ -5,9 +5,9 @@ import unittest
 
 
 # Define the environment class
-class SimpleDotGameEnv(gym.Env):
+class DroneEnv(gym.Env):
     def __init__(self):
-        super(SimpleDotGameEnv, self).__init__()
+        super(DroneEnv, self).__init__()
         
         # Define action and observation space
         # Actions: 0 = up, 1 = down, 2 = left, 3 = right
@@ -17,7 +17,7 @@ class SimpleDotGameEnv(gym.Env):
         self.observation_space = spaces.Box(low=0, high=8, shape=(2,), dtype=np.int32)
         
         # Initial positions
-        self.player_position = np.array([1, 1])
+        self.player_position = np.array([0, 0])
         self.flag_position = np.array([8, 8])
         self.obstacles = [
             np.array([3, 3]),
@@ -93,45 +93,3 @@ class SimpleDotGameEnv(gym.Env):
 #         obs, reward, done, info = env.step(action)
 #         env.render()
 #         print(f"Action: {action}, Reward: {reward}")
-
-# Unit test for the environment
-class TestSimpleDotGameEnv(unittest.TestCase):
-    def setUp(self):
-        self.env = SimpleDotGameEnv()
-
-    # def test_initial_state(self):
-    #     obs = self.env.reset()
-    #     self.assertTrue((obs == np.array([1, 1])).all())
-    #     self.assertFalse(self.env.done)
-
-    # def test_step_up(self):
-    #     self.env.reset()
-    #     obs, reward, done, _ = self.env.step(0)  # Move up
-    #     expected_position = np.array([1, 0])
-    #     self.assertTrue((obs == expected_position).all())
-    #     self.assertAlmostEqual(reward, -0.1)
-    #     self.assertFalse(done)
-
-    # def test_step_to_obstacle(self):
-    #     self.env.reset()
-    #     self.env.player_position = np.array([3, 2])  # Set position near an obstacle
-    #     obs, reward, done, _ = self.env.step(1)  # Move down to hit obstacle
-    #     self.assertTrue(done)
-    #     self.assertAlmostEqual(reward, -1)
-
-    # def test_reach_flag(self):
-    #     self.env.reset()
-    #     self.env.player_position = np.array([8, 7])  # Set position near the flag
-    #     obs, reward, done, _ = self.env.step(1)  # Move down to reach the flag
-    #     self.assertTrue(done)
-    #     self.assertAlmostEqual(reward, 10)
-
-    def test_go_out_from_the_range(self):
-        self.env.reset()
-        self.env.player_position = np.array([1, 1])
-        _, reward, done, _ = self.env.step(0)
-        print(self.env.player_position)
-        self.env.render()
-
-if __name__ == "__main__":
-    unittest.main(argv=[''], exit=False)
