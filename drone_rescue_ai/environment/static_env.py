4ba24340586e74bef3ae6c5b340c051672688e47
Evgenii Iurin <iurin.evgenii@gmail.com>
2024-11-17 23:01:28 +0100

    Add target point

--- drone_rescue_ai/environment/static_env.py
+++ drone_rescue_ai/environment/static_env.py
@@ -15,8 +15,9 @@ class Environment():
             4: (10, 2),
         }
         self.color_map = {
-            0: 'white', 
-            1: 'black'
+            0: 'white', # background
+            1: 'black', # obstacle
+            2: 'red' # target point
         }
         self._generate_area()
 
@@ -26,25 +27,26 @@ class Environment():
         obstacles_x = np.random.randint(0, self.area_size, self.num_obstacles)
         obstacles_y = np.random.randint(0, self.area_size, self.num_obstacles)
         
-        # Extend obstacles
+        target_point_x = np.random.randint(0, self.area_size)
+        target_point_y = np.random.randint(0, self.area_size)
+        
+        # Add obstacles
         for x, y in zip(obstacles_x, obstacles_y):
             sampled_obstacle = self.obstacles_map[
                 np.random.choice(list(self.obstacles_map.keys()))
                 ]
-            self._insert_kernel(sampled_obstacle, (x, y))
+            self._insert_kernel(sampled_obstacle, (x, y), 1)
+        
+        # Add target point
+        self._insert_kernel((3, 3), (target_point_x, target_point_y), 2)
 
-    def _insert_kernel(self, kernel_shape: tuple[int, int], point: tuple[int, int]):
+    def _insert_kernel(self, kernel_shape: tuple[int, int], point: tuple[int, int], fill_with: int):
         kernel_center = (kernel_shape[0] // 2, kernel_shape[1] // 2)
-
-        # Calculate the bounds for applying the kernel
         start_row = max(point[0] - kernel_center[0], 0)
         end_row = min(point[0] + kernel_center[0] + 1, self.area.shape[0])
-
         start_col = max(point[1] - kernel_center[1], 0)
         end_col = min(point[1] + kernel_center[1] + 1, self.area.shape[1])
-
-        # Apply the kernel to the array
-        self.area[start_row:end_row, start_col:end_col] = 1
+        self.area[start_row:end_row, start_col:end_col] = fill_with
 
 
     def save_env_as_image(self, save_path):
